1ecd1112fed35728a5044d65b449d6d80484a80d
Peter Kiraly <peter.kiraly@gwdg.de>
2016-02-15 11:07:11 +0100

    Rename App to PicaplusToJson

--- src/main/java/com/nsdr/pica/PicaplusToJson.java
+++ src/main/java/com/nsdr/pica/PicaplusToJson.java
@@ -13,25 +13,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Hello world!
+ * 
  *
  */
-public class App {
+public class PicaplusToJson {
 
-    private static final Logger log = Logger.getLogger(App.class.getCanonicalName());
+    private static final Logger log = Logger.getLogger(PicaplusToJson.class.getCanonicalName());
     private static final Pattern p = Pattern.compile("^\\x1e([^ ]+) \\x1f(.*?)$");
     private static final ObjectMapper mapper = new ObjectMapper(new JsonFactory());
 
     public static void main(String[] args) {
-        System.out.println("Hello World!" + args[0]);
         String file = args[0];
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             Record record = new Record();
             while ((line = br.readLine()) != null) {
-                // process the line.
-                // System.err.println(String.format("line: %s (%d)", line, line.length()));
                 Matcher m = p.matcher(line);
                 if (m.find()) {
                     String code = m.group(1);
@@ -53,9 +50,7 @@ public class App {
     
     private static void printRecord(Record record) {
         try {
-            System.out.println("==========[ RECORD ]==============");
-            String jsonInString = mapper.writeValueAsString(record);
-            System.out.println(jsonInString);
+            System.out.println(mapper.writeValueAsString(record));
         } catch (JsonProcessingException ex) {
             log.severe(ex.getLocalizedMessage());
         }
